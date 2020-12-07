1cad94fe3a4a9f398703ee0d6262a6ee9639b720
Konstantin <balashov-92@mail.ru>
2020-12-07 15:32:46 +0300

    add FavoriteAdmin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Recipe, Tag, Ingredient, IngredientAmount, Follow, \
+    Favorite
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'pub_date', 'duration',)
+    search_fields = ('title', 'description')
+    list_filter = ('pub_date',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'style', 'slug')
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'dimension',)
+    search_fields = ('title',)
+    list_filter = ('title', 'dimension')
+
+
+@admin.register(IngredientAmount)
+class IngredientAmountAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'ingredient', 'amount')
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    search_fields = ('user', 'author')
+    list_filter = ('user',)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user')
+    search_fields = ('user',)
+    list_filter = ('user',)
