83f315a65bc700e3a07d47c50555242546ab611c
Konstantin <balashov-92@mail.ru>
2020-12-12 22:12:54 +0300

    add handlers to handle tags_list and generate own links to each tag

---
+++ recipes/handlers.py
@@ -0,0 +1,44 @@
+from recipes.models import Tag
+
+
+def generate_tags_list(tag_slug, slugs_list, default_slugs):
+    if tag_slug in slugs_list:
+        slugs_list.remove(tag_slug)
+    else:
+        index = default_slugs.index(tag_slug)
+        if len(slugs_list) <= index:
+            slugs_list.append(tag_slug)
+        else:
+            slugs_list.insert(index, tag_slug)
+
+
+def tags_handler(search, tags_list=[]):
+    default_slugs = [tag.slug for tag in Tag.objects.all()]
+    if search:
+        tags_list.clear()
+        for tag_slug in search:
+            if tag_slug in tags_list:
+                tags_list.remove(tag_slug)
+            else:
+                tags_list.append(tag_slug)
+        return tags_list
+    if tags_list:
+        return tags_list
+    return default_slugs
+
+
+def generate_tag_links(tags_list):
+    default_slugs = [tag.slug for tag in Tag.objects.all()]
+    links = {}
+    for tag in default_slugs:
+        if tag in tags_list:
+            links[tag] = '&'.join(
+                [f'search={new_tag}' for new_tag in tags_list if
+                 new_tag != tag])
+            if not links[tag]:
+                links[tag] = f'search= '
+        else:
+            links[tag] = '&'.join(
+                [f'search={new_tag}' for new_tag in default_slugs if
+                 new_tag in tags_list or new_tag == tag])
+    return links
