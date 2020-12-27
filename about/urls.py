c8e0adcd048ccf0b09dda9669e7a8aeee5bec481
Konstantin <balashov-92@mail.ru>
2020-12-27 04:25:15 +0300

    added new urls

--- about/urls.py
+++ about/urls.py
@@ -2,7 +2,7 @@ from about import views
 from django.urls import path
 
 urlpatterns = [
-    path('author/', views.JustStaticPage.as_view(), name='about_author'),
-    path('tech/', views.JustStaticPage.as_view(), name='about_tech')
+    path('author/', views.AboutAuthorView.as_view(), name='about_author'),
+    path('tech/', views.AboutTechView.as_view(), name='about_tech')
 
 ]
