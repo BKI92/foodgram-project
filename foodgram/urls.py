652c8d3b4e833b0aee826065135de95623ea4559
Konstantin <balashov-92@mail.ru>
2020-12-27 04:25:47 +0300

    added urls to about app

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("auth/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
+    path("about/", include("about.urls"))
 
 ]
 
