d70727b5358a51d45c01cd9f1a97a379b8f666fb
Konstantin <balashov-92@mail.ru>
2021-01-09 00:12:56 +0300

    created manager command to load ingredients from csv to database

---
+++ recipes/management/commands/load_ingredients_data.py
@@ -0,0 +1,17 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Load ingredients data to database'
+
+    def handle(self, *args, **options):
+        with open('recipes/fixtures/ingredients.csv', encoding='Utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                try:
+                    title, dimension = row
+                    Ingredient.objects.get_or_create(title=title, dimension=dimension)
+                except Exception as exc:
+                    print(f'В строке {row} некорректные данные')
