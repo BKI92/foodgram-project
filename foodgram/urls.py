c5b42c0ab66e8c0380f850b685a872e8e0cf6734
Konstantin <balashov-92@mail.ru>
2021-01-15 17:18:08 +0300

    added handlers to 404 and 500, updated imports

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -13,18 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import handler404, handler500
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.flatpages import views
+from django.urls import include, path
 
 from foodgram import settings
+
+handler404 = "recipes.views.page_not_found"
+handler500 = "recipes.views.server_error"
 
 urlpatterns = [
     path("auth/", include("users.urls")),
     path("auth/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
-    path("about/", include("about.urls"))
+]
+
+urlpatterns += [
+    path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about_author'),
+    path('about-tech/', views.flatpage, {'url': '/about-tech/'}, name='about_tech'),
 
 ]
 
