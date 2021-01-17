ac9355896736cc60819988d43279b835e36fc91d
BKI92 <52736034+BKI92@users.noreply.github.com>
2021-01-17 19:43:32 +0300

    Delete about directory

--- about/urls.py
+++
@@ -1,8 +0,0 @@
-from about import views
-from django.urls import path
-
-urlpatterns = [
-    path('author/', views.AboutAuthorView.as_view(), name='about_author'),
-    path('tech/', views.AboutTechView.as_view(), name='about_tech')
-
-]
