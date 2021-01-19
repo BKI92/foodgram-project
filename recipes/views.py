74b01b92f087b5f11e8656840176077992ad626e
Konstantin <balashov-92@mail.ru>
2021-01-19 13:27:36 +0300

    delete blank lines

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import FileResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic.base import TemplateView
 
 from foodgram.settings import VISIBLE_RECIPES
 from recipes.forms import RecipeForm
@@ -353,8 +352,3 @@ def delete_favorites(request, recipe_id):
             my_favorite.recipes.remove(recipe_id)
             return JsonResponse({'success': True})
     return JsonResponse({'success': False})
-
-
-
-
-
