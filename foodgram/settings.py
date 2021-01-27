ba587566565e75f2b7760161f2427be184829a10
Konstantin <balashov-92@mail.ru>
2021-01-27 17:07:52 +0300

    updated

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -33,7 +33,7 @@ SITE_ID = 5
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '178.154.254.140',
-                 'www.pretty-food.tk', 'www.pretty-food.tk']
+                 'pretty-food.tk', 'www.pretty-food.tk']
 # Application definition
 
 INSTALLED_APPS = [
