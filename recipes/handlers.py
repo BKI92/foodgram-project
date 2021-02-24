2e86c37ee9821dd88579662a1c72ac6fe06d0801
Konstantin <balashov-92@mail.ru>
2021-02-24 16:40:49 +0300

    update method of getting ingredients from form

--- recipes/handlers.py
+++ recipes/handlers.py
@@ -73,8 +73,17 @@ def generate_pdf(ingredients, buffer):
 
 def get_form_ingredients(request):
     ingredients = {}
-    for key, ingredient_name in request.POST.items():
+    for key, value in request.POST.items():
         if 'nameIngredient' in key:
             ing_id = key.split('_')[-1]
-            ingredients[ingredient_name] = int(ing_id)
+            name = value
+            ingredients[ing_id] = [name]
+        if 'valueIngredient' in key:
+            ing_id = key.split('_')[-1]
+            amount = value
+            ingredients[ing_id].append(amount)
+        if 'unitsIngredient' in key:
+            ing_id = key.split('_')[-1]
+            units = value
+            ingredients[ing_id].append(units)
     return ingredients
